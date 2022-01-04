884ef077efa926c69003450aed6dd17dcd9e66e0
abduljeleelng <abduljeleelng@gmail.com>
2022-01-04 22:07:34 +0100

    React-native mapbox real time tracking demo

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+// import type {Node} from 'react';
+
+const App: () => Node = () => {
+  // const isDarkMode = useColorScheme() === 'dark';
+
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
+
+  return (
+    <View>
+      <Text>Hello word !</Text>
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({
+
+// });
+
+export default App;
